4ad108b4798f57844d4f06520943a112a05a446b
SeungHoon-Song <s10269@naver.com>
2021-01-09 15:39:14 +0900

    plus textfield MouseEvent

--- PR/src/model/MainFrame.java
+++ PR/src/model/MainFrame.java
@@ -2,8 +2,13 @@ package model;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,13 +16,14 @@ import javax.swing.JPanel;
 public class MainFrame extends JFrame {
 
 	public MainFrame() {
-		setTitle("Swing TCP Chatting Test");
+		setTitle("Chatting Test");
 		setBounds(10, 50, 400, 250);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(0, 2));
 		JButton btnServer = new JButton("Server");
 		JButton btnClient = new JButton("Client");
+		TextField tftext = new TextField("테스트");
 		// 버튼클릭 액션에 반응하기 위해 리스너 객체 생성 및 추가
 
 		btnServer.addActionListener(new ActionListener() {
@@ -25,10 +31,19 @@ public class MainFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// ServerFrame 객체 생성
-				ServerFrame frame = new ServerFrame();
+				ChatServer frame = new ChatServer();
 
 			}
 
+		});
+		tftext.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ChatServer.id=tftext.getText();
+				ChatClient.id=tftext.getText();
+				
+			}
 		});
 
 		btnClient.addActionListener(new ActionListener() {
@@ -36,7 +51,7 @@ public class MainFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// ClientFrame 객체 생성
-				ClientFrame frame = new ClientFrame();
+				ChatClient frame = new ChatClient();
 
 			}
 
@@ -44,6 +59,8 @@ public class MainFrame extends JFrame {
 
 		panel.add(btnServer);
 		panel.add(btnClient);
+		tftext.setBounds(170, 164, 50, 20);
+		add(tftext);
 		add(panel, BorderLayout.CENTER);
 		setVisible(true);
 
